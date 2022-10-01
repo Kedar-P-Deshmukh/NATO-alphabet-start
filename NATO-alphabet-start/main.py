fcbd2361026994dc08a8dae39ec1edc684913682
Kedar <mr.kedardeshmukh@gmail.com>
2022-10-01 22:29:18 +0530

    second commit

--- NATO-alphabet-start/main.py
+++ NATO-alphabet-start/main.py
@@ -5,4 +5,4 @@ data=pandas.read_csv("nato_phonetic_alphabet.csv")
 natodict= { rowdata.letter:rowdata.code for index,rowdata in data.iterrows()}
 name=input("enter your name:")
 print([natodict[alpha.upper()] for alpha in name])
-#
+#ddds
